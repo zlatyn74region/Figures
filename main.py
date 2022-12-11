8a3c62ef688182331e3ece135fb8c7460197bb03
zlatyn74region <zlatyn74region@mail.ru>
2022-12-11 23:52:40 +0500

    Дополнение проекта

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
 from models import *
+from prettytable import PrettyTable
+from typing import List
 
-p1 = Point(1, 2)
-p2 = Point(3, 4)
+def print_table(figures: List[Figure]) -> None:
+    table = PrettyTable(['Номер', 'Название', 'Периметр', 'Площадь'])
 
-len = Figure.get_len(p1, p2)
-print(len)
+    for i in range(len(figures)):
+        table.add_row([i + 1, figures[i], round(figures[i].get_perimeter(), 2), round(figures[i].get_area(), 2)])
+    
+    print(table)
 
-p3 = Point(6, 8)
-fig = Triangle(p1, p2, p3)
+if __name__ == '__main__':
+    figures = list()
+    points = list() 
 
-per = fig.get_perimeter()
-area = fig.get_area()
+    amount_figures = int(input("Введите количество нужных фигур: "))
 
-print(per)
-print(area)
+    while amount_figures:
+        type_figure = input("Введите тип фигуры (triangle/rectangle/circle): ")
+
+        if type_figure == 'triangle':
+            print("Треугольник")
+            
+            x1, y1, x2, y2, x3, y3 = map(int, input(f"Введите координаты трех точек (через пробел: x1 y1 ...): ").split())
+
+            type_figure = Triangle(Point(x1,y1), Point(x2,y2), Point(x3,y3))
+        
+        if type_figure == 'rectangle':
+            print("Четырехугольник")
+
+            x1, y1, x2, y2, x3, y3 = map(int, input(f"Введите координаты трех точек (через пробел: x1 y1 ...): ").split())
+
+            type_figure = Rectangle(Point(x1,y1), Point(x2,y2), Point(x3,y3))
+            
+        if type_figure == 'circle':
+            print("Круг")
+
+            x1, y1, x2, y2 = map(int, input(f"Введите координаты двух точек (через пробел: x1 y1 ...): ").split())
+
+            type_figure = Circle(Point(x1,y1), Point(x2,y2))
+            
+        figures.append(type_figure)
+
+        amount_figures -= 1
+
+        if not amount_figures: 
+            break
+
+    print_table(figures=figures)
--- models.py
+++ models.py
@@ -4,26 +4,25 @@ from abc import ABC, abstractmethod
 
 
 class Point():
-    x : float
-    y : float
+    x: float
+    y: float
 
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
+def get_len(p1: Point, p2: Point) -> float:
+    return math.sqrt(math.pow(p2.x - p1.x, 2) + math.pow(p2.y - p1.y, 2))
+
 class Figure(ABC):
     @abstractmethod
-    def get_perimeter():
+    def get_perimeter() -> float:
         pass
     
     @abstractmethod
-    def get_area():
+    def get_area() -> float:
         pass
     
-    @staticmethod
-    def get_len(p1 : Point, p2 : Point):
-        return math.sqrt(math.pow(p2.x - p1.x, 2) + math.pow(p2.y - p1.y, 2))
-
     @abstractmethod
     def __str__(self) -> str:
         pass
@@ -34,14 +33,14 @@ class Triangle(Figure):
     len_p3_p1 : float
 
     def __init__(self, p1 : Point, p2 : Point, p3 : Point):
-        self.len_p1_p2 = super().get_len(p1, p2)
-        self.len_p2_p3 = super().get_len(p2, p3)
-        self.len_p3_p1 = super().get_len(p3, p1)
+        self.len_p1_p2 = get_len(p1, p2)
+        self.len_p2_p3 = get_len(p2, p3)
+        self.len_p3_p1 = get_len(p3, p1)
 
-    def get_perimeter(self):
+    def get_perimeter(self) -> float:
         return self.len_p1_p2 + self.len_p2_p3 + self.len_p3_p1
 
-    def get_area(self):
+    def get_area(self) -> float:
         semi_perimeter = self.get_perimeter() / 2
         return math.sqrt(semi_perimeter * (semi_perimeter - self.len_p1_p2) 
                                         * (semi_perimeter - self.len_p2_p3)
@@ -49,3 +48,35 @@ class Triangle(Figure):
 
     def __str__(self) -> str:
         return 'Треугольник'
+
+class Rectangle(Figure):
+    len_p1_p2 : float
+    len_p2_p3 : float
+    
+    def __init__(self, p1 : Point, p2 : Point, p3 : Point):
+        self.len_p1_p2 = get_len(p1, p2)
+        self.len_p2_p3 = get_len(p2, p3)
+
+    def get_perimeter(self) -> float:
+        return (self.len_p1_p2 + self.len_p2_p3)*2
+
+    def get_area(self) -> float:
+        return self.len_p1_p2 * self.len_p2_p3
+
+    def __str__(self) -> str:
+        return 'Прямоугольник'
+
+class Circle(Figure):
+    len_p1_p2 : float
+    
+    def __init__(self, p1 : Point, p2 : Point):
+        self.len_p1_p2 = get_len(p1, p2)
+
+    def get_perimeter(self) -> float:
+        return 2 * self.len_p1_p2 * math.pi
+
+    def get_area(self) -> float:
+        return math.pi*math.pow(self.len_p1_p2, 2)
+
+    def __str__(self) -> str:
+        return 'Круг'
